cc7e60b9c2fe89b5d58ca77a4bc02ca167ed5a5c
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-06-20 18:05:02 +0900

    Add a case

--- test_fixed.bash
+++ test_fixed.bash
@@ -18,6 +18,9 @@ tmp=/tmp/$$
 [ "$1" == "nobuild" ] || cargo build || err $LINENO
 cd "$test_dir"
 
+res=$($com <<< 'a[ ]=0; echo ${a[0]}')
+[ "$res" = '0' ] || err $LINENO
+
 res=$($com <<< 'A=#a ; echo $A')
 [ "$res" = '#a' ] || err $LINENO
 
