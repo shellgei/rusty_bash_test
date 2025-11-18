4cfaa79a6607432416d786c4586a1013bfef6ba4
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-11-18 20:23:39 +0900

    Add a test case

---
+++ test_fixed.bash
@@ -0,0 +1,28 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Ryuichi Ueda ryuichiueda@gmail.com
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+err () {
+	echo $0 >> ./error
+	echo "ERROR!" FILE: $0, LINENO: $1
+	rm -f $tmp-*
+	exit 1
+}
+
+repo_dir=${2:-~/GIT/rusty_bash}
+test_dir="$PWD"
+com="$repo_dir/target/debug/sush"
+cd "$repo_dir"
+tmp=/tmp/$$
+
+[ "$1" == "nobuild" ] || cargo build || err $LINENO
+cd "$test_dir"
+
+#res=$($com <<< 'declare -A v=( a b ); declare -p v; echo "${v[@]@k}"')
+#[ "$res" = 'declare -A v=([a]="b" )
+#a b' ] || err $LINENO
+
+rm -f $tmp-*
+echo $0 >> ./ok
+exit
+
