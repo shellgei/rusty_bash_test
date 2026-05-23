5e0d51b88c8aee87533e44e83d519955240ef5c4
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-05-23 15:36:30 +0900

    Relocate

--- test.bash
+++ test.bash
@@ -49,6 +49,7 @@ cd "$test_dir"
 ./test_fixed_v1.2.5.bash nobuild "$repo_dir" &
 ./test_fixed_v1.2.6.bash nobuild "$repo_dir" &
 ./test_fixed_v1.2.7.bash nobuild "$repo_dir" &
+./test_fixed_v1.2.9.bash nobuild "$repo_dir" &
 
 wait 
 
--- test_fixed.bash
+++ test_fixed.bash
@@ -18,51 +18,51 @@ tmp=/tmp/$$
 [ "$1" == "nobuild" ] || cargo build || err $LINENO
 cd "$test_dir"
 
-res=$($com <<< 'enable | wc -l')
-[ "$res" -gt 10 ] || err $LINENO
-
-res=$($com <<< 'enable -n readonly ; enable | grep readonly')
-[ "$?" -ne 0 ] || err $LINENO
-
-if [ "$(uname)" = Linux ] ; then
-	res=$($com <<< 'ulimit -n 256; ulimit -n')
-	[ "$res" = "256" ] || err $LINENO
-fi
-
-res=$($com <<< 'read -ru3 x 3< <(echo bbb); echo $x')
-[ "$res" = "bbb" ] || err $LINENO
-
-res=$($com <<< 'A=abc; echo ${A::}')
-[ "$?" -eq 0 ] || err $LINENO
-[ "$res" = "" ] || err $LINENO
-
-res=$($com <<< 'A=abc; echo ${A::2}')
-[ "$?" -eq 0 ] || err $LINENO
-[ "$res" = "ab" ] || err $LINENO
-
-res=$($com <<< 'echo ${a"}')
-[ "$?" -eq 2 ] || err $LINENO
-
-if [ "$(uname)" = Linux ] ; then
-	res=$($com <<< 'coproc REFLECT { cat - ; } ; kill $REFLECT_PID; sleep 1')
-	ps u | grep '[c]at -'
-	[ "$?" -eq 1 ] || { killall cat ; err $LINENO ; }
-fi
-
-res=$($com <<< 'exec 4>/tmp/exec4 ; echo aaa >&4; cat /tmp/exec4; rm /tmp/exec4')
-[ "$res" = "aaa" ] || err $LINENO
-
-res=$($com <<< 'exec 3>/tmp/exec3 ; echo aaa >&3; cat /tmp/exec3; rm /tmp/exec3')
-[ "$res" = "aaa" ] || err $LINENO
-
-res=$($com <<< 'exec 5>/tmp/exec5; exec 4<&5-; echo hoge >&4; cat /tmp/exec5 ;rm /tmp/exec5')
-[ "$res" = "hoge" ] || err $LINENO
-
-res=$($com <<< 'coproc cat - ; exec 3<&${COPROC[1]}- ; echo ${COPROC[@]}')
-[ "$res" = "63 -1" ] || err $LINENO
-
-res=$($com <<< 'coproc cat - ; exec 3>&${COPROC[0]}- ; echo ${COPROC[@]}')
-[ "$res" = "-1 60" ] || err $LINENO
+#res=$($com <<< 'enable | wc -l')
+#[ "$res" -gt 10 ] || err $LINENO
+#
+#res=$($com <<< 'enable -n readonly ; enable | grep readonly')
+#[ "$?" -ne 0 ] || err $LINENO
+#
+#if [ "$(uname)" = Linux ] ; then
+#	res=$($com <<< 'ulimit -n 256; ulimit -n')
+#	[ "$res" = "256" ] || err $LINENO
+#fi
+#
+#res=$($com <<< 'read -ru3 x 3< <(echo bbb); echo $x')
+#[ "$res" = "bbb" ] || err $LINENO
+#
+#res=$($com <<< 'A=abc; echo ${A::}')
+#[ "$?" -eq 0 ] || err $LINENO
+#[ "$res" = "" ] || err $LINENO
+#
+#res=$($com <<< 'A=abc; echo ${A::2}')
+#[ "$?" -eq 0 ] || err $LINENO
+#[ "$res" = "ab" ] || err $LINENO
+#
+#res=$($com <<< 'echo ${a"}')
+#[ "$?" -eq 2 ] || err $LINENO
+#
+#if [ "$(uname)" = Linux ] ; then
+#	res=$($com <<< 'coproc REFLECT { cat - ; } ; kill $REFLECT_PID; sleep 1')
+#	ps u | grep '[c]at -'
+#	[ "$?" -eq 1 ] || { killall cat ; err $LINENO ; }
+#fi
+#
+#res=$($com <<< 'exec 4>/tmp/exec4 ; echo aaa >&4; cat /tmp/exec4; rm /tmp/exec4')
+#[ "$res" = "aaa" ] || err $LINENO
+#
+#res=$($com <<< 'exec 3>/tmp/exec3 ; echo aaa >&3; cat /tmp/exec3; rm /tmp/exec3')
+#[ "$res" = "aaa" ] || err $LINENO
+#
+#res=$($com <<< 'exec 5>/tmp/exec5; exec 4<&5-; echo hoge >&4; cat /tmp/exec5 ;rm /tmp/exec5')
+#[ "$res" = "hoge" ] || err $LINENO
+#
+#res=$($com <<< 'coproc cat - ; exec 3<&${COPROC[1]}- ; echo ${COPROC[@]}')
+#[ "$res" = "63 -1" ] || err $LINENO
+#
+#res=$($com <<< 'coproc cat - ; exec 3>&${COPROC[0]}- ; echo ${COPROC[@]}')
+#[ "$res" = "-1 60" ] || err $LINENO
 
 rm -f $tmp-*
 echo $0 >> ./ok
